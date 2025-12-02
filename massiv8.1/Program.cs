b45975631873c80c9101a01fbb3b8c9cd3222a58
232417 <232417@DESKTOP-1K0QRTR>
2025-12-02 13:15:47 +0300

    Добавьте файлы проекта.

---
+++ massiv8.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massiv8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] ms = new int[4, 4];
+            int num = 1;
+            for (int i = 0; i < ms.GetLength(0); i++)
+            {
+                for (int j = 0; j < ms.GetLength(1); j++)
+                {
+                    ms[i, j] = num;
+                    num++;
+                    Console.Write(ms[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
